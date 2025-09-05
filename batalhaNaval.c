79b843a5eadd6d1ba8adf455ed52c1d358543da6
Franklin Andrade <100174287+frk-andrade@users.noreply.github.com>
2025-09-05 19:42:47 +0000

    Finalizado o nível novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,11 +4,101 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
+    int linha[] = {1,2,3,4,5,6,7,8,9,10}; //Indicadores de linhas
+    char coluna[] = {'A','B','C','D','E','F','G','H','I','J'}; // Indicadores de colunas
+
+    //Matriz do tabuleiro
+
+    int tabuleiro[10][10] = {
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 }
+    };
+
+    printf("Tabuleiro Batalha Naval\n");
+    printf(" ");
+
+    //Loop para exibir as letras que indicam as colunas
+    for (int j = 0; j < 10; j++){
+        printf(" %c", coluna[j]);
+    };
+
+    printf("\n"); // Para quebrar a linha
+
+    //Loop para numerar as linhas
+    for (int i = 0; i < 10; i++){
+        printf("%d ", linha[i]);
+            //Loop aninhado que insere as linhas da matriz após as numerações de linhas
+            for (int j = 0;  j < 10;  j++)
+            {
+                printf("%d ", tabuleiro[i][j]);
+            }
+
+        printf("\n"); // Apenas para quebrar linha    
+        
+    };
+    int linhaEscolhida = 1;
+    int colunaInicial = 4;
+    
+    //Navio na horizontal
+    if(linhaEscolhida > 10) { //Condição para não ultrapassar a quantidade de linhas do tabuleiro
+        printf("Insira um valor menor que 10.\n");
+    } else{
+    for (int j = 0;j < 3; j++){
+            tabuleiro[linhaEscolhida][colunaInicial + j] = 3;
+    }}
+
+    int colunaEscolhida = 6;
+    int linhaInicial = 3;
+
+    //Navio na vertical
+     if(colunaEscolhida > 10) { //Condição para não ultrapassar a quantidade de colunas do tabuleiro
+        printf("Insira um valor menor que 10.\n");
+    } else if (colunaInicial == linhaInicial) //Condição para evitar que os navios se choquem
+    {
+        printf("Calma comndante! Já tem um navio aí!\n"); 
+    } else {
+    for (int i = 0;i < 3; i++){
+            tabuleiro[linhaInicial + i][colunaEscolhida] = 3;
+    }}
+
+    printf("Tabuleiro Batalha Naval\n");
+    
+    printf(" ");
+
+    //Loop para exibir as letras que indicam as colunas
+    for (int j = 0; j < 10; j++){
+        printf(" %c", coluna[j]);
+    };
+
+    printf("\n"); // Para quebrar a linha
+
+    //Loop para numerar as linhas
+    for (int i = 0; i < 10; i++){
+        printf("%d ", linha[i]);
+            //Loop aninhado que insere as linhas da matriz após as numerações de linhas
+            for (int j = 0;  j < 10;  j++)
+            {
+                printf("%d ", tabuleiro[i][j]);
+            }
+
+        printf("\n"); // Apenas para quebrar linha    
+        
+    };
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
